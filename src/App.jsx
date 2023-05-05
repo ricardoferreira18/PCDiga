0501c34755fab8054bf3776a86c3ddeb45784cf1
Ricardo <ricardogf@outlook.pt>
2023-05-05 11:47:25 +0100

    footer not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
-import Footer from "./components/footer/footer";
 import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
